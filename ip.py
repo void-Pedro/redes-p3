6a6296fb5f2ab2f4d8dbe19b65b603093c3bb439
lmathaeus <lmathaeus@hotmail.com>
2024-01-10 12:22:11 -0300

    Add files via upload

--- ip.py
+++ ip.py
@@ -1,5 +1,5 @@
 from iputils import *
-
+from ipaddress import ip_network, ip_address
 
 class IP:
     def __init__(self, enlace):
@@ -13,6 +13,8 @@ class IP:
         self.enlace.registrar_recebedor(self.__raw_recv)
         self.ignore_checksum = self.enlace.ignore_checksum
         self.meu_endereco = None
+        self.tabela = None
+        self.enviar_identificacao = 0
 
     def __raw_recv(self, datagrama):
         dscp, ecn, identification, flags, frag_offset, ttl, proto, \
@@ -25,13 +27,38 @@ class IP:
             # atua como roteador
             next_hop = self._next_hop(dst_addr)
             # TODO: Trate corretamente o campo TTL do datagrama
-            self.enlace.enviar(datagrama, next_hop)
+            ttl -= 1
+            ip_ver = 4
+            ip_vhl = 5
+            ip_ver = (ip_ver << 4 ) + ip_vhl
+            if ttl > 0:
+                datagrama = struct.pack('!BBHHHBBHII', ip_ver, dscp | ecn, len(datagrama), identification, flags | frag_offset, ttl, proto, 0, int.from_bytes(str2addr(src_addr), 'big'), int.from_bytes(str2addr(dst_addr), 'big'))
+                datagrama = struct.pack('!BBHHHBBHII', ip_ver, dscp | ecn, len(datagrama), identification, flags | frag_offset, ttl, proto, calc_checksum(datagrama), int.from_bytes(str2addr(src_addr), 'big'), int.from_bytes(str2addr(dst_addr), 'big'))
+                self.enlace.enviar(datagrama, next_hop)
+            else:
+                next_hop = self._next_hop(src_addr)
+                erro = struct.pack('!BBHHHBBHII', ip_ver, dscp | ecn, 48, identification, flags | frag_offset, 64, IPPROTO_ICMP, 0, int.from_bytes(str2addr(self.meu_endereco), 'big'), int.from_bytes(str2addr(src_addr), 'big'))
+                erro = struct.pack('!BBHHHBBHII', ip_ver, dscp | ecn, 48, identification, flags | frag_offset, 64, IPPROTO_ICMP, calc_checksum(erro), int.from_bytes(str2addr(self.meu_endereco), 'big'), int.from_bytes(str2addr(src_addr), 'big'))
+                time_exceeded = struct.pack("!BBHHH", 11, 0, 0, 0, 0)
+                time_exceeded = struct.pack('!BBHHH', 11, 0, calc_checksum(erro + time_exceeded), 0, 0) + datagrama[:28]
+                mensagem_icmp = erro + time_exceeded
+                #print(len(mensagem_icmp))
+                self.enlace.enviar(mensagem_icmp, next_hop)
+
 
     def _next_hop(self, dest_addr):
         # TODO: Use a tabela de encaminhamento para determinar o próximo salto
         # (next_hop) a partir do endereço de destino do datagrama (dest_addr).
-        # Retorne o next_hop para o dest_addr fornecido.
-        pass
+        # Retorne o next_hop para o dest_addr fornecido
+        ip = None
+        maior_prefix = -1
+        for cdr in self.tabela:
+            net = ip_network(cdr[0])
+            if (ip_address(dest_addr) in net) and (net.prefixlen > maior_prefix):
+                maior_prefix = net.prefixlen
+                ip = cdr[1]
+
+        return ip
 
     def definir_endereco_host(self, meu_endereco):
         """
@@ -51,7 +78,8 @@ class IP:
         """
         # TODO: Guarde a tabela de encaminhamento. Se julgar conveniente,
         # converta-a em uma estrutura de dados mais eficiente.
-        pass
+        self.tabela = tabela
+
 
     def registrar_recebedor(self, callback):
         """
@@ -67,4 +95,14 @@ class IP:
         next_hop = self._next_hop(dest_addr)
         # TODO: Assumindo que a camada superior é o protocolo TCP, monte o
         # datagrama com o cabeçalho IP, contendo como payload o segmento.
+        
+        
+        ip_ver = 4
+        ip_vhl = 5
+        ip_ver = (ip_ver << 4 ) + ip_vhl
+
+        datagrama = struct.pack('!BBHHHBBHII',ip_ver, 0, len(segmento)+20, self.enviar_identificacao,0,64,6, 0,int.from_bytes(str2addr(self.meu_endereco), 'big'), int.from_bytes(str2addr(dest_addr), 'big'))
+        datagrama = struct.pack('!BBHHHBBHII',ip_ver, 0, len(segmento)+20, self.enviar_identificacao,0,64,6, calc_checksum(datagrama),int.from_bytes(str2addr(self.meu_endereco), 'big'), int.from_bytes(str2addr(dest_addr), 'big'))
+        datagrama = datagrama + segmento
+        self.enviar_identificacao += 1
         self.enlace.enviar(datagrama, next_hop)
